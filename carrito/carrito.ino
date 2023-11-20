449ea04b794aead56916075932f4c6f08d04d555
rodolfodom <rodolfodom7@gmail.com>
2023-11-20 05:57:08 -0600

    nRF24l01 configuration done

--- carrito/carrito.ino
+++ carrito/carrito.ino
@@ -1,9 +1,74 @@
+#include <RF24.h>
+#include <RF24_config.h>
+#include <nRF24L01.h>
+#include <printf.h>
+
+#include <SPI.h>
+
+RF24 radio(2, 5); 
+const uint64_t address = 0xF0F0F0F0E1LL;  
+
+struct MyData 
+{
+  int counter;
+  float temperature;
+  float humidity;
+  float altitude;
+  float pressure;
+};
+MyData data;
+
+
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  radio.begin();
+  radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
+  radio.setPALevel(RF24_PA_MAX);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.startListening();
+
 
 }
 
+
+int recvData()
+{
+  if ( radio.available() ) 
+  {
+    radio.read(&data, sizeof(MyData));
+    return 1;
+    }
+    return 0;
+}
+
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(recvData()){
+ 
+    Serial.println("Data Received:");
+    Serial.print("Packet No. = ");
+    Serial.println(data.counter);
+    
+    Serial.print("Temperature = ");
+    Serial.print(data.temperature);
+    Serial.println("*C");
+  
+    Serial.print("Pressure = ");
+    Serial.print(data.pressure);
+    Serial.println("hPa");
+  
+    Serial.print("Approx. Altitude = ");
+    Serial.print(data.altitude);
+    Serial.println("m");
+  
+    Serial.print("Humidity = ");
+    Serial.print(data.humidity);
+    Serial.println("%");
+  
+    Serial.println();
+  }
 
 }
--- control/control.ino
+++ control/control.ino
@@ -1,9 +1,58 @@
+#include <RF24.h>
+#include <RF24_config.h>
+#include <nRF24L01.h>
+#include <printf.h>
+
+#include <SPI.h>
+
+#define CE_PIN 9
+#define CSN_PIN 10
+const uint64_t address = 0xF0F0F0F0E1LL;
+RF24 radio(CE_PIN, CSN_PIN);
+int counter = 0;
+
+struct MyData 
+{
+  int counter;
+  float temperature;
+  float humidity;
+  float altitude;
+  float pressure;
+};
+
+MyData data;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  radio.begin();                  //Starting the Wireless communication
+  radio.openWritingPipe(address); //Setting the address where we will send the data
+  radio.setPALevel(RF24_PA_MAX);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.stopListening(); 
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  data.counter = counter;
+  data.temperature = 1;
+  data.pressure = 2;
+  data.altitude = 3;
+  data.humidity = 4;
 
+  Serial.print("Packet No. = ");
+  Serial.println(data.counter);
+
+  bool ok = radio.write(&data, sizeof(MyData));
+
+  if(ok)
+  {
+     Serial.println("------------------- paquete enviado -------------"); 
+  }
+  else
+  {
+     Serial.println("********************** no se ha podido enviar *******************");
+  }
+
+  counter++;
+  delay(5000);
 }
